a42a34b2e998698ab896f7f0dc34b4d5bc9989d8
michalsut <sut.michal@gmail.com>
2023-08-17 17:52:29 +0200

    Rename package

--- tests/base/settings.py
+++ tests/base/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django_admin_shortcuts",
+    "django_handy_admin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
